283d71f1211b056a5d44df19e8fd5dd0ea682fbf
Bayu Mido Saputra <bayumbayu0708@gmail.com>
2026-09-07 11:18:53 +0700

    mendeskripsikan wajah

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import sys
 
 print("Praktek Pemrograman Terapan - Pertemuan 1")
-print("Nama :", "Bayu Mido Saputra")
+print("Nama :", "Bayu Mido Saputra ganteng")
 print("NIM  :", "33132501002")
 print("Python:", sys.version)
